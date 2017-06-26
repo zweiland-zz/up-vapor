503b80d09395a84bbe5a84932fe274c39eadc962
Zak Weiland <zweiland@gmail.com>
2017-06-26 16:44:46 -0500

    content in correct place

--- src/components/shell/appWrap/AppWrap.js
+++ src/components/shell/appWrap/AppWrap.js
@@ -7,7 +7,7 @@ export default class AppWrap extends Component {
     console.log(process.env.UP_UI_THEME);
     // const BRAND = process.env.UP_UI_THEME;
     return (
-      <div className={baseStyles.app + " " + theme.app}>
+      <div className={baseStyles.appWrap + " " + theme.appWrap}>
         {this.props.children}
       </div>
     )
