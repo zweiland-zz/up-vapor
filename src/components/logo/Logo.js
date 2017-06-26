faf40ddfae47d4bdc62fce5013f2c739ece934bf
Zak Weiland <zweiland@gmail.com>
2017-06-26 10:54:21 -0500

    logo start

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -4,10 +4,28 @@ import baseStyles from './styles/base.css';
 export default class Logo extends Component {
   render(props) {
     var { theme } = this.props;
+    console.log(process.env.UP_UI_THEME);
+    const BRAND = process.env.UP_UI_THEME;
     return (
       <div className={baseStyles.logo + " " + theme.logo}>
         <div className={baseStyles.brandName + " " + theme.brandName}>
-          Logo
+
+          {BRAND === 'base' &&
+          <div>
+            Brand Name
+          </div>
+          }
+          {BRAND === 'themeA' &&
+          <div>
+            <img src="/images/bluehost-logo.svg" alt="Bluehost Logo" />
+          </div>
+          }
+          {BRAND === 'themeB' &&
+          <div>
+            <img src="/images/ipage-logo.svg" alt="iPage Logo" />
+          </div>
+          }
+
         </div>
       </div>
     )
